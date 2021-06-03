a3e1b80c1c6e0a0fa949e284c4b40d2d33e08b20
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-06-03 09:17:51 +0545

    creatig books with coreData

---
+++ project 11/AddBookView.swift
@@ -0,0 +1,72 @@
+//
+//  AddBookView.swift
+//  project 11
+//
+//  Created by Anisha Lamichhane on 6/2/21.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror" , "Kids" , "Mystery", "Romance", "Poetry", "Thriller", "Suspense"]
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of Book: ", text: $title)
+                    TextField("Author's Name: ", text: $author)
+                     
+                    Picker("Genre: ", selection: $genre){
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Rating: ", selection: $rating) {
+                        ForEach(0..<6){
+                            Text("\($0)")
+                        }
+                    }
+                    
+                    TextField("Write a review:", text: $review)
+                }
+                
+                Section {
+                    Button("Save") {
+//                        create an instance of the Book class using our managed object context  
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author =  self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
+                        try? self.moc.save()
+                        
+//                        to dismiss the sheet
+                        self.presentationMode.wrappedValue.dismiss()
+                        
+                    }
+                }
+            }
+            .navigationBarTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
--- project 11/ContentView.swift
+++ project 11/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Text("Count: \(books.count)")
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showingAddScreen.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                        }
+                )
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView().environment(\.managedObjectContext, self.moc)
+                }
+        }
     }
 }
 
